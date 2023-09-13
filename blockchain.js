14f219800643ccee42adb0d883ecf873b3cc7f4e
Mazi Tobe <emma.too50@googlemail.com>
2023-09-13 18:20:45 +0100

    updating code as I go

--- blockchain.js
+++ blockchain.js
@@ -1,10 +1,37 @@
 const SHA256= require('crypto-js/sha256');
+const EC= require('elliptic').ec;
+const ec= new EC('secp256k1');
 
 class Transactions{
     constructor(fromAddress, toAddress, amount){
         this.fromAddress= fromAddress;
         this.toAddress= toAddress;
         this.amount= amount;
+    }
+
+    calculateHash(){
+        return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+    }
+
+    signTransaction(){
+        if(signingKey.getPublic('hex') != this.fromAddress){
+            throw new Error('You are not authorized to sign for this wallet!!!');
+        }
+
+        const hashTranx= this.calculateHash();
+        const signER= signingKey.sign(hashTranx, 'base64');
+        this.signature= signER.toDER('hex');
+    }
+
+    isValid(){
+        if(this.fromAddress === null) return true;
+
+        if(!this.signature || this.signature.length === 0){
+            throw new Error('No signature was provided for this transaction!!!');
+        }
+
+        const publicKey= ec.keyFromPublic(this.fromAddress, 'hex');
+        return publicKey.verify(this.calculateHash(), this.signature);
     }
 }
 
